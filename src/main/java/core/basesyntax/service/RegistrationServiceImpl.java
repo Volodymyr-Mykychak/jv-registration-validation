10e5640e025a7e896c3554234a53e94d3e3dc579
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-06-22 16:51:53 +0300

    make task

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -15,7 +15,6 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_USER_AGE = 18;
     private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
-
     /**
      * Реєструє користувача, виконуючи перевірки та додаючи користувача до сховища.
      *
@@ -24,40 +23,44 @@ public class RegistrationServiceImpl implements RegistrationService {
      * @throws UserRegistrationException якщо реєстрація користувача не вдалась
      * @throws PasswordException         якщо пароль не відповідає вимогам
      */
+//    @Override
+//    public User register(User user) {
+//        userValidate(user);
+//        return storageDao.add(user);
+//    }
 
     @Override
     public User register(User user) {
-        userValidate(user);
-        return storageDao.add(user);
+        return null;
     }
 
-    /**
-     * Виконує перевірку користувача.
-     *
-     * @param user користувач для перевірки
-     * @throws UserRegistrationException якщо перевірка користувача не вдалась
-     * @throws PasswordException         якщо пароль не відповідає вимогам
-     */
-    private void userValidate(User user) {
-        if (user == null) {
-            throw new UserRegistrationException("User cannot be null");
-        }
-        if (user.getLogin() == null) {
-            throw new UserRegistrationException("Login must be filled");
-        }
-        if (user.getAge() == null) {
-            throw new UserRegistrationException("Age must be filled");
-        }
-        if (user.getAge() < MIN_USER_AGE) {
-            throw new UserRegistrationException("the user is too young. His age should be "
-                    + MIN_USER_AGE);
-        }
-        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new PasswordException("Password length is incorrect."
-                    + " The length of the password is correct " + MIN_PASSWORD_LENGTH);
-        }
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new UserRegistrationException("This user with login already exists");
-        }
-    }
+//    /**
+//     * Виконує перевірку користувача.
+//     *
+//     * @param user користувач для перевірки
+//     * @throws UserRegistrationException якщо перевірка користувача не вдалась
+//     * @throws PasswordException         якщо пароль не відповідає вимогам
+//     */
+//    private void userValidate(User user) {
+//        if (user == null) {
+//            throw new UserRegistrationException("User cannot be null");
+//        }
+//        if (user.getLogin() == null) {
+//            throw new UserRegistrationException("Login must be filled");
+//        }
+//        if (user.getAge() == null) {
+//            throw new UserRegistrationException("Age must be filled");
+//        }
+//        if (user.getAge() < MIN_USER_AGE) {
+//            throw new UserRegistrationException("the user is too young. His age should be "
+//                    + MIN_USER_AGE);
+//        }
+//        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+//            throw new PasswordException("Password length is incorrect."
+//                    + " The length of the password is correct " + MIN_PASSWORD_LENGTH);
+//        }
+//        if (storageDao.get(user.getLogin()) != null) {
+//            throw new UserRegistrationException("This user with login already exists");
+//        }
+//    }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -29,160 +29,160 @@ class RegistrationServiceTest {
     /**
      * Метод, який очищає базу даних після кожного тесту.
      */
-    @AfterEach
-    void clearStorage() {
-        Storage.people.clear();
-    }
+//    @AfterEach
+//    void clearStorage() {
+//        Storage.people.clear();
+//    }
 
     /**
      * Метод {@code register_normalUser_ok} тестує реєстрацію нормального користувача
      * з коректними даними. Перевіряється правильність поверненого результату,
      * наявність ідентифікатора після реєстрації та стан бази даних.
      */
-    @Test
-    void register_normalUser_ok() {
-        // Створення нормального користувача з коректними даними
-        User normalUser = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
-        // Виклик методу реєстрації користувача
-        User registeredUser = registrationService.register(normalUser);
-        // Перевірка, що повернутий користувач є тим самим, що і вхідний
-        Assertions.assertSame(normalUser, registeredUser,
-                "register method must return same user as was supplied");
-        // Перевірка, що після реєстрації користувач має ідентифікатор
-        assertNotNull(normalUser.getId(), "User must have id after registration");
-        // Перевірка стану бази даних
-        List<User> actualDbState = new ArrayList<>();
-        actualDbState.add(normalUser);
-        assertEquals(Storage.people, actualDbState, "User must be saved in db");
-    }
+//    @Test
+//    void register_normalUser_ok() {
+//        // Створення нормального користувача з коректними даними
+//        User normalUser = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
+//        // Виклик методу реєстрації користувача
+//        User registeredUser = registrationService.register(normalUser);
+//        // Перевірка, що повернутий користувач є тим самим, що і вхідний
+//        Assertions.assertSame(normalUser, registeredUser,
+//                "register method must return same user as was supplied");
+//        // Перевірка, що після реєстрації користувач має ідентифікатор
+//        assertNotNull(normalUser.getId(), "User must have id after registration");
+//        // Перевірка стану бази даних
+//        List<User> actualDbState = new ArrayList<>();
+//        actualDbState.add(normalUser);
+//        assertEquals(Storage.people, actualDbState, "User must be saved in db");
+//    }
 
     /**
      * Тест на реєстрацію декількох звичайних користувачів.
      * Перевіряє, чи користувачі були зареєстровані успішно, чи мають вони ID після реєстрації
      * та чи були користувачі збережені у базі даних.
      */
-    @Test
-    void register_fewNormalUsers_ok() {
-        // Очікуваний стан бази даних
-        List<User> expectedDbState = new ArrayList<>();
-
-        // Реєстрація кількох нормальних користувачів
-        for (int i = 0; i < 100; i++) {
-            // Створення нормального користувача з унікальними даними
-            User normalUser = createUser(DEFAULT_LOGIN + i, VALID_PASSWORD + i, MIN_USER_AGE + i);
-            // Виклик методу реєстрації користувача
-            registrationService.register(normalUser);
-            // Додавання користувача до очікуваного стану бази даних
-            expectedDbState.add(normalUser);
-        }
-        // Перевірка, що всі користувачі мають ідентифікатори після реєстрації
-        for (User user : expectedDbState) {
-            assertNotNull(user.getId(), "User must have id after registration");
-        }
-        // Перевірка стану бази даних
-        assertEquals(Storage.people, expectedDbState, "Unexpected database state");
-    }
+//    @Test
+//    void register_fewNormalUsers_ok() {
+//        // Очікуваний стан бази даних
+//        List<User> expectedDbState = new ArrayList<>();
+//
+//        // Реєстрація кількох нормальних користувачів
+//        for (int i = 0; i < 100; i++) {
+//            // Створення нормального користувача з унікальними даними
+//            User normalUser = createUser(DEFAULT_LOGIN + i, VALID_PASSWORD + i, MIN_USER_AGE + i);
+//            // Виклик методу реєстрації користувача
+//            registrationService.register(normalUser);
+//            // Додавання користувача до очікуваного стану бази даних
+//            expectedDbState.add(normalUser);
+//        }
+//        // Перевірка, що всі користувачі мають ідентифікатори після реєстрації
+//        for (User user : expectedDbState) {
+//            assertNotNull(user.getId(), "User must have id after registration");
+//        }
+//        // Перевірка стану бази даних
+//        assertEquals(Storage.people, expectedDbState, "Unexpected database state");
+//    }
 
     /**
      * Тест на реєстрацію користувача з вказаним ID.
      * Перевіряє, чи користувач був зареєстрований успішно та чи був він збережений у базі даних.
      */
-    @Test
-    void register_withId_ok() {
-        // Створення користувача з вказаним ідентифікатором
-        User user = createUser(-1L, DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
-        // Реєстрація користувача
-        registrationService.register(user);
-        // Очікуваний стан бази даних
-        List<User> expectedDbState = new ArrayList<>();
-        expectedDbState.add(user);
-        // Перевірка, що користувач був збережений у базі даних
-        assertEquals(Storage.people, expectedDbState, "User must be saved in db");
-    }
+//    @Test
+//    void register_withId_ok() {
+//        // Створення користувача з вказаним ідентифікатором
+//        User user = createUser(-1L, DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
+//        // Реєстрація користувача
+//        registrationService.register(user);
+//        // Очікуваний стан бази даних
+//        List<User> expectedDbState = new ArrayList<>();
+//        expectedDbState.add(user);
+//        // Перевірка, що користувач був збережений у базі даних
+//        assertEquals(Storage.people, expectedDbState, "User must be saved in db");
+//    }
 
     /**
      * Метод {@code register_sameLogin_notOk} тестує реєстрацію користувача з вже існуючим логіном.
      * Перевіряється виникнення винятку {@code UserRegistrationException} та правильність стану бази даних
      * після спроби реєстрації користувача з вже існуючим логіном.
      */
-    @Test
-    void register_sameLogin_notOk() {
-        // Створення першого користувача з унікальним логіном
-        User normalUser1 = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
-        // Створення другого користувача з тим самим логіном, але з іншими даними
-        User normalUser2 = createUser(DEFAULT_LOGIN, VALID_PASSWORD + VALID_PASSWORD, MIN_USER_AGE * 2);
-        // Реєстрація першого користувача
-        registrationService.register(normalUser1);
-        // Перевірка, що при спробі реєстрації другого користувача з вже існуючим логіном
-        // виникає виняток UserRegistrationException
-        assertThrows(UserRegistrationException.class, () -> registrationService.register(normalUser2));
-        // Очікуваний стан бази даних - містить тільки першого користувача
-        List<User> expectedDbState = new ArrayList<>();
-        expectedDbState.add(normalUser1);
-        // Перевірка стану бази даних
-        assertEquals(Storage.people, expectedDbState, "Only first user must be saved in db");
-    }
+//    @Test
+//    void register_sameLogin_notOk() {
+//        // Створення першого користувача з унікальним логіном
+//        User normalUser1 = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE);
+//        // Створення другого користувача з тим самим логіном, але з іншими даними
+//        User normalUser2 = createUser(DEFAULT_LOGIN, VALID_PASSWORD + VALID_PASSWORD, MIN_USER_AGE * 2);
+//        // Реєстрація першого користувача
+//        registrationService.register(normalUser1);
+//        // Перевірка, що при спробі реєстрації другого користувача з вже існуючим логіном
+//        // виникає виняток UserRegistrationException
+//        assertThrows(UserRegistrationException.class, () -> registrationService.register(normalUser2));
+//        // Очікуваний стан бази даних - містить тільки першого користувача
+//        List<User> expectedDbState = new ArrayList<>();
+//        expectedDbState.add(normalUser1);
+//        // Перевірка стану бази даних
+//        assertEquals(Storage.people, expectedDbState, "Only first user must be saved in db");
+//    }
 
     /**
      * Метод {@code register_userTooYoung_notOk} тестує реєстрацію надто молодого користувача.
      * Перевіряється виникнення винятку {@code UserRegistrationException} та правильність стану бази даних
      * після спроби реєстрації надто молодого користувача.
      */
-    @Test
-    void register_userTooYoung_notOk() {
-        // Створення користувача з надто молодим віком
-        User youngUser = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE - 1);
-        // Перевірка, що при спробі реєстрації надто молодого користувача виникає виняток UserRegistrationException
-        assertThrows(UserRegistrationException.class, () -> registrationService.register(youngUser));
-        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
-        List<User> expectedDbState = new ArrayList<>();
-        // Перевірка стану бази даних
-        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
-    }
+//    @Test
+//    void register_userTooYoung_notOk() {
+//        // Створення користувача з надто молодим віком
+//        User youngUser = createUser(DEFAULT_LOGIN, VALID_PASSWORD, MIN_USER_AGE - 1);
+//        // Перевірка, що при спробі реєстрації надто молодого користувача виникає виняток UserRegistrationException
+//        assertThrows(UserRegistrationException.class, () -> registrationService.register(youngUser));
+//        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
+//        List<User> expectedDbState = new ArrayList<>();
+//        // Перевірка стану бази даних
+//        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
+//    }
 
     /**
      * Метод {@code register_nullFields_notOk} тестує реєстрацію користувача з нульовими полями.
      * Перевіряється виникнення винятку {@code UserRegistrationException} та правильність стану бази даних
      * після спроби реєстрації користувача з нульовими полями.
      */
-    @Test
-    void register_insecurePassword_notOk() {
-        // Створення користувача з небезпечним паролем (порожній пароль)
-        User user = createUser(DEFAULT_LOGIN, "", MIN_USER_AGE);
-        // Перевірка, що при спробі реєстрації користувача з небезпечним паролем виникає виняток PasswordException
-        assertThrows(PasswordException.class, () -> registrationService.register(user));
-        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
-        List<User> expectedDbState = new ArrayList<>();
-        // Перевірка стану бази даних
-        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
-    }
+//    @Test
+//    void register_insecurePassword_notOk() {
+//        // Створення користувача з небезпечним паролем (порожній пароль)
+//        User user = createUser(DEFAULT_LOGIN, "", MIN_USER_AGE);
+//        // Перевірка, що при спробі реєстрації користувача з небезпечним паролем виникає виняток PasswordException
+//        assertThrows(PasswordException.class, () -> registrationService.register(user));
+//        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
+//        List<User> expectedDbState = new ArrayList<>();
+//        // Перевірка стану бази даних
+//        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
+//    }
 
     /**
      * Тест на реєстрацію користувача з нульовими полями.
      * Перевіряє, чи виникає виняток UserRegistrationException під час реєстрації користувача з нульовими полями,
      * та чи не зберігається користувач у базі даних.
      */
-    @Test
-    void register_nullFields_notOk() {
-        // Створення користувача з нульовими полями
-        User user = createUser(null, null, null);
-        // Перевірка, що при спробі реєстрації користувача з нульовими полями виникає виняток UserRegistrationException
-        assertThrows(UserRegistrationException.class, () -> registrationService.register(user));
-        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
-        List<User> expectedDbState = new ArrayList<>();
-        // Перевірка стану бази даних
-        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
-    }
+//    @Test
+//    void register_nullFields_notOk() {
+//        // Створення користувача з нульовими полями
+//        User user = createUser(null, null, null);
+//        // Перевірка, що при спробі реєстрації користувача з нульовими полями виникає виняток UserRegistrationException
+//        assertThrows(UserRegistrationException.class, () -> registrationService.register(user));
+//        // Очікуваний стан бази даних - порожній, користувач не повинен бути збережений
+//        List<User> expectedDbState = new ArrayList<>();
+//        // Перевірка стану бази даних
+//        assertEquals(Storage.people, expectedDbState, "User mustn't be saved");
+//    }
 
     /**
      * Метод {@code register_null_notOk} тестує реєстрацію з нульовим користувачем.
      * Перевіряється виникнення винятку {@code UserRegistrationException} при спробі реєстрації нульового користувача.
      */
-    @Test
-    void register_null_notOk() {
-        // Перевірка, що при спробі реєстрації нульового користувача виникає виняток UserRegistrationException
-        assertThrows(UserRegistrationException.class, () -> registrationService.register(null));
-    }
+//    @Test
+//    void register_null_notOk() {
+//        // Перевірка, що при спробі реєстрації нульового користувача виникає виняток UserRegistrationException
+//        assertThrows(UserRegistrationException.class, () -> registrationService.register(null));
+//    }
 
     /**
      * Приватний метод {@code createUser} використовується для створення об'єкту користувача з заданими параметрами.
@@ -195,9 +195,9 @@ class RegistrationServiceTest {
      * @param age      вік користувача
      * @return об'єкт користувача з заданими параметрами
      */
-    private User createUser(String login, String password, Integer age) {
-        return createUser(null, login, password, age);
-    }
+//    private User createUser(String login, String password, Integer age) {
+//        return createUser(null, login, password, age);
+//    }
 
     /**
      * Приватний метод {@code createUser} використовується для створення об'єкту користувача з заданими параметрами.
@@ -210,12 +210,12 @@ class RegistrationServiceTest {
      * @param age      вік користувача
      * @return об'єкт користувача з заданими параметрами
      */
-    private User createUser(Long id, String login, String password, Integer age) {
-        User user = new User();
-        user.setId(id);
-        user.setLogin(login);
-        user.setPassword(password);
-        user.setAge(age);
-        return user;
-    }
+//    private User createUser(Long id, String login, String password, Integer age) {
+//        User user = new User();
+//        user.setId(id);
+//        user.setLogin(login);
+//        user.setPassword(password);
+//        user.setAge(age);
+//        return user;
+//    }
 }
